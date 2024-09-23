40f98e59794c5931ccdb193f48891511da6f89bc
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-23 11:37:43 -0300

    gsdhfg

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
 
-var dadosPaulo = socument.querySelector('#linhaPaulo');
+var dadosPaulo = document.querySelector('#linhaPaulo');
 var peso = dadosPaulo.query.querySelector('.info-peso').textContent;
 var altura = dadosPaulo.querySelector('.info-altura').textContent;
 var imc = peso / (altura * altura);
