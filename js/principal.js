99c964f65366233489e0fd02535ff3188545cb2a
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-23 11:42:09 -0300

    dfjg

--- js/principal.js
+++ js/principal.js
@@ -5,4 +5,4 @@ var dadosPaulo = document.querySelector('#linhaPaulo');
 var peso = dadosPaulo.query.querySelector('.info-peso').textContent;
 var altura = dadosPaulo.querySelector('.info-altura').textContent;
 var imc = peso / (altura * altura);
-dadosPaulo.querySelector('.info-imc').textContent = imc
+dadosPaulo.querySelector('.info-imc').textContent = imc;
