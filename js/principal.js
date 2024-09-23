4c00425c2de6a0626f0350b7c17ba3825ee0702f
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-23 11:49:52 -0300

    bbbb

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,8 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
 
-var paulo = document.querySelector('.paciente1');
-var peso = dadosPaulo.query.querySelector('.info-peso').textContent;
-var altura = dadosPaulo.querySelector('.info-altura').textContent;
+var paulo = document.querySelector('#paulo');
+var peso = paulo.query.querySelector('.info-peso').textContent;
+var altura = paulo.querySelector('.info-altura').textContent;
 var imc = peso / (altura * altura);
 paulo.querySelector('.info-imc').textContent = imc;
